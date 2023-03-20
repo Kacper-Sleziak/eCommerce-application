a4d949ac24c0429dcdd0b2b3332be1ee1eb65b11
Kacper Śleziak <kacper-sleziak@wp.pl>
2023-03-20 20:05:02 +0100

    Switched JS -> TS

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import Home from './pages/Home'
 
-// @ts-ignore
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Home />
