b8e5cda8f8ee4b8f160aeb8b82800d377dc035c9
Oldzii <olga23284@gmail.com>
2023-03-23 20:59:28 +0100

    test

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Home(): JSX.Element {
-  return <div>Home branch mock master test</div>
+  return <div>Home branch mock master test test</div>
 }
 
 export default Home
