238ec2aff19ceaae3647f6967d93feb1c764f7d0
Kacper Śleziak <kacpersleziak00@gmail.com>
2023-04-02 18:23:20 +0200

    Router protecntion improved and material UI theme setup

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Button } from '@mui/material'
 
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <div>Home</div>
+    </div>
+  )
 }
 
 export default Home
