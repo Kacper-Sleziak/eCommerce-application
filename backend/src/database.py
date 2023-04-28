d548415cdf45101fb7fa79185694dd47cffea2fa
Kacper Śleziak <kacpersleziak00@gmail.com>
2023-04-28 17:24:35 +0200

    Rebase

---
+++ backend/src/database.py
@@ -0,0 +1,9 @@
+# import psycopg2
+
+
+# conn = psycopg2.connect(database="leasing_shop",
+#                         host="sql",
+#                         user="postgres",
+#                         password="postgres",
+#                         port="5432")
+# database = "database"
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,32 +1,7 @@
-import logging
-import time
-import traceback
-from threading import Thread
-
-import uvicorn
 from fastapi import FastAPI
-from api.main_api import MainApi
 
+app = FastAPI()
 
-def run_fastapi_thread(api: FastAPI) -> None:
-    while True:
-        try:
-            time.sleep(1)
-            uvicorn.run(api, host="0.0.0.0", port=9999)
-        except Exception:
-            logging.critical("Fast API exception %s", traceback.format_exc())
-
-
-def run_fast_api() -> None:
-    api = MainApi()
-
-    fast_api_thread = Thread(
-        name="Fast API thread", target=run_fastapi_thread, args=(api.backend_api,)
-    )
-
-    fast_api_thread.start()
-
-
-if __name__ == "__main__":
-    run_fast_api()
-    print("Successfully running")
+@app.get("/")
+async def welcome() -> dict:    
+    return { "message": "Hello World!"}
