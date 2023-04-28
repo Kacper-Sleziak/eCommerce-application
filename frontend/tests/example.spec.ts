174bb9868a2064033f6152b57be8f19da3c57342
Oldzii <olga23284@gmail.com>
2023-04-28 14:53:40 +0200

    should fail in ci

--- frontend/tests/example.spec.ts
+++ frontend/tests/example.spec.ts
@@ -1,12 +1,10 @@
 import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/')
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)
+test('find local page', async ({ page }) => {
+  // await page.goto('https://playwright.dev/')
+  await page.goto('http://localhost:3000')
 })
 
-test('get started link', async ({ page }) => {
+test('find playwright page ', async ({ page }) => {
   await page.goto('https://playwright.dev/')
 })
