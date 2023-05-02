9b1977f4e77513e4bd29dc294a9fdeef2476bdc9
oluchna <olek@rafkos.net>
2023-05-03 01:27:46 +0200

    Offer list page added without filters

---
+++ frontend/src/components/UsedFilter.jsx
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types'
+import { Box, Card, Typography } from '@mui/material'
+
+const UsedFilter = ({ filtername, filterdetail }) => {
+  return (
+    <Box>
+      <Typography
+        sx={{
+          fontStyle: 'normal',
+          fontWeight: 600,
+          fontSize: '20px',
+          lineHeight: '23px',
+          color: '#000',
+        }}
+      >
+        {filtername}
+      </Typography>
+      <Card
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          boxSizing: 'border-box',
+          width: '158px',
+          height: '51px',
+          left: '81px',
+          top: '310px',
+          background: '#FCA311',
+          borderRadius: '10px',
+        }}
+      >
+        <Typography
+          sx={{
+            fontStyle: 'normal',
+            fontWeight: 600,
+            fontSize: '20px',
+            lineHeight: '23px',
+            color: '#000',
+          }}
+        >
+          {filterdetail}
+        </Typography>
+      </Card>
+    </Box>
+  )
+}
+
+UsedFilter.propTypes = {
+  filtername: PropTypes.string.isRequired,
+  filterdetail: PropTypes.string.isRequired,
+}
+
+export default UsedFilter
