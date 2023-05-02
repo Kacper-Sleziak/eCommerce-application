b73713496517497ab737448fd7aedf746288eb57
oluchna <olek@rafkos.net>
2023-05-02 22:44:40 +0200

    Home page added

--- frontend/src/components/InputMui.jsx
+++ frontend/src/components/InputMui.jsx
@@ -1,24 +1,16 @@
-import * as React from 'react'
+import PropTypes from 'prop-types'
 import TextField from '@mui/material/TextField'
 import InputAdornment from '@mui/material/InputAdornment'
-// seach in input or in diffrent component?
 import SearchIcon from '@mui/icons-material/Search'
 
-interface InputProps {
-  title: string
-  search?: boolean
-}
-
-// add basic value for search
-const InputMui = ({ title, search }: InputProps) => {
-  // Unexpected nullable boolean value in conditional. Please handle the nullish case explicitly
+const InputMui = ({ title, search }) => {
   return (
     <TextField
       id="input-with-icon-textfield"
       label={title}
       variant="outlined"
       InputProps={
-        search
+        search != null && search
           ? {
               startAdornment: (
                 <InputAdornment position="start">
@@ -34,6 +26,11 @@ const InputMui = ({ title, search }: InputProps) => {
 
 InputMui.defaultProps = {
   search: false,
+}
+
+InputMui.propTypes = {
+  title: PropTypes.string.isRequired,
+  search: PropTypes.bool,
 }
 
 export default InputMui
--- frontend/src/components/OfferShortcut.jsx
+++ frontend/src/components/OfferShortcut.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import { Card, CardMedia, CardContent, Typography } from '@mui/material'
 import '../styles/home.css'
 
-const OfferShortcut = () => {
-  return <div className="offerDiv">Hello</div>
+const OfferShortcut = ({ imageUrl, title, price }) => {
+  return (
+    <Card
+      sx={{
+        maxWidth: 345,
+        padding: 2,
+        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        borderRadius: '30px',
+        backgroundColor: '#E6E6E6',
+        '&:hover': {
+          background: '#a9a9a9',
+        },
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="300px"
+        width="250px"
+        image={imageUrl}
+        alt="Image"
+        sx={{ borderRadius: '30px' }}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          {title}
+        </Typography>
+        <Typography variant="h6">{price}</Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
+OfferShortcut.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
 }
 
 export default OfferShortcut
