24e5aa711238ec21a0eadf4394ea5a9b59da0258
CaptainBoii <smieciowymailbartka@gmail.com>
2023-05-12 18:34:46 +0200

    Added color and brand

--- backend/docker/dump.sql
+++ backend/docker/dump.sql
@@ -21,6 +21,11 @@ CREATE TABLE role (
     name varchar(255) NOT NULL
 );
 
+CREATE TABLE color (
+    color_id serial PRIMARY KEY,
+    name varchar(255) NOT NULL
+);
+
 CREATE TABLE user_ (
     user_id serial PRIMARY KEY,
     role_id int NOT NULL,
@@ -56,6 +61,7 @@ CREATE TABLE product (
     product_id serial PRIMARY KEY,
     seller_id int NOT NULL,
     name varchar(255) NOT NULL,
+    brand varchar(255) NOT NULL,
     product_description varchar(255) NOT NULL,
     quantity int NOT NULL,
     total_price decimal(8,2) NOT NULL,
@@ -86,6 +92,14 @@ CREATE TABLE product_category (
     FOREIGN KEY (product_id) REFERENCES product(product_id)
 );
 
+CREATE TABLE product_color (
+    product_id int NOT NULL,
+    color_id int NOT NULL,
+    PRIMARY KEY (product_id, color_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id),
+    FOREIGN KEY (color_id) REFERENCES color(color_id)
+);
+
 CREATE TABLE order_product (
     order_id int NOT NULL,
     product_id int NOT NULL,
@@ -113,19 +127,23 @@ INSERT INTO address(country, region, city, postal_code, street, building, flat, 
 
 INSERT INTO role(name) VALUES ('Admin'), ('User');
 
+INSERT INTO color(name) VALUES ('Blue'), ('Red'), ('Yellow'), ('White'), ('Black'), ('Green');
+
 INSERT INTO user_(role_id, address_id, username, email, password) VALUES (1, 1, 'adm', 'adm@pwr.pl', 'p0o9i8u7'), (2, 1, 'pickle', 'pickle@pwr.pl', '1q2w3e4r5t');
 
 INSERT INTO sale_order(buyer_id, seller_id, order_date, total_price) VALUES (2, 1, '2023-03-03', 599.98);
 
 INSERT INTO review(seller_id, reviewer_id, review, review_description) VALUES (1, 2, 8, 'Pretty good');
 
-INSERT INTO product(seller_id, name, product_description, quantity, total_price, sale_type) VALUES (1, 'Office chair', 'Pretty comfy chair', 6, 299.99, 'Regular'), (1, 'Volvo V4', 'Not used much to be honest', 1, 52299.99, 'Regular'), (1, 'Tesla T100', 'Exploited, barely usable', 1, 263299.99, 'Regular'), (1, 'Bentley B1', 'A classic, pristine condition. No scratches, not even used. Without tires', 1, 300000.00, 'Auction');
+INSERT INTO product(seller_id, name, brand, product_description, quantity, total_price, sale_type) VALUES (1, 'Office chair', 'Basic', 'Pretty comfy chair', 6, 299.99, 'Regular'), (1, 'V4', 'Volvo', 'Not used much to be honest', 1, 52299.99, 'Regular'), (1, 'T100', 'Tesla', 'Exploited, barely usable', 1, 263299.99, 'Regular'), (1, 'B1', 'Bentley', 'A classic, pristine condition. No scratches, not even used. Without tires', 1, 300000.00, 'Auction');
 
 INSERT INTO question_answer(product_id, question, answer) VALUES (4, 'Is it red?', 'It is white');
 
 INSERT INTO photo(photo_url, product_id) VALUES ('/backend/static/1/6ab79ff3-2eb8-46e7-b6e8-0a29fc3396a2.jpg', 1), ('/backend/static/2/volvo.webp', 2), ('/backend/static/3/tesla.webp', 3), ('/backend/static/4/bentlus.jpg', 4);
 
 INSERT INTO product_category(category_id, product_id) VALUES (3, 1), (1, 2), (1, 3), (1, 4);
+
+INSERT INTO product_color(color_id, product_id) VALUES (1, 1), (2, 2), (3, 3), (4, 4);
 
 INSERT INTO order_product(order_id, product_id, quantity) VALUES (1, 1, 2);
 
--- backend/src/app/product/service.py
+++ backend/src/app/product/service.py
@@ -1,8 +1,8 @@
 import os
 from typing import List
 import uuid
-from app.models import CreateEngine, Product, ProductCategory, Category, Photo
-from app.utils import query_to_dict, product_to_json, category_to_json, photo_to_json
+from app.models import CreateEngine, Product, ProductCategory, Category, Photo, Color, ProductColor
+from app.utils import query_to_dict, product_to_json, category_to_json, photo_to_json, color_to_json
 from sqlalchemy.dialects import postgresql
 from sqlalchemy import or_, and_, desc, asc
 from app.product.models import ProductCreate
@@ -22,6 +22,7 @@ class ProductService:
         Session = self.engine.create_session()
         with Session() as session:
             filters = list()
+
             if params.has_search():
                 filters.append(Product.name.like("%{}%".format(params.search)))
             if params.has_quantity():
@@ -31,21 +32,22 @@ class ProductService:
                 for category in params.categories:
                     categories.append(ProductCategory.category_id == category)
                 filters.append(or_(*categories))
-            # if params.has_brands():
-            #     brands = list()
-            #     for brand in params.brands:
-            #         brands.append(Product.brand == brand)
-            #     filters.append(or_(*brands))
-            # if params.has_colors():
-            #     colors = list()
-            #     for color in params.colors:
-            #         colors.append(ProductColors.color_id == color)
-            #     filters.append(or_(*colors))
+            if params.has_brands():
+                brands = list()
+                for brand in params.brands:
+                    brands.append(Product.brand == brand)
+                filters.append(or_(*brands))
+            if params.has_colors():
+                colors = list()
+                for color in params.colors:
+                    colors.append(Color.name == color)
+                filters.append(or_(*colors))
             if params.has_price():
                 filters.append(Product.total_price < params.price)
+
             sort = asc(params.order_by) if params.order == "ASC" else desc(params.order_by)
-            query = session.query(Product).join(ProductCategory).filter(and_(*filters)).order_by(sort)
-            products = query.limit(params.limit).offset(params.page * params.limit).all()
+            query = session.query(Product).join(ProductCategory).join(ProductColor).join(Color).filter(and_(*filters))
+            products = query.order_by(sort).limit(params.limit).offset(params.page * params.limit)
             for product in products:
                 result[count] = self.get_product_info(product)
                 count += 1
@@ -68,15 +70,18 @@ class ProductService:
         return result
 
     def get_product(self, product_id: int) -> dict:
+
         Session = self.engine.create_session()
         with Session() as session:
             product = session.query(Product).filter(Product.product_id == product_id).one()
             result = self.get_product_info(product)
         Session.remove()
+
         return result
 
     def get_product_categories(self, product_id: int) -> dict:
         result = dict()
+
         Session = self.engine.create_session()
         with Session() as session:
             categories = session.query(Category).join(ProductCategory).filter(
@@ -84,33 +89,53 @@ class ProductService:
             for i, category in enumerate(categories):
                 result[i] = category_to_json(category)
         Session.remove()
+
+        return result
+
+    def get_product_colors(self, product_id: int) -> str:
+        result = dict()
+
+        Session = self.engine.create_session()
+        with Session() as session:
+            colors = session.query(Color).join(ProductColor).filter(
+                ProductColor.product_id == product_id).all()
+            for i, color in enumerate(colors):
+                result[i] = color_to_json(color)
+        Session.remove()
+
         return result
 
     def get_product_photos(self, product_id: int) -> str:
         result = dict()
+
         Session = self.engine.create_session()
         with Session() as session:
             photos = session.query(Photo).filter(Photo.product_id == product_id).all()
             for i, photo in enumerate(photos):
                 result[i] = photo_to_json(photo)
         Session.remove()
+
         return result
 
     def get_product_info(self, product: Product) -> dict:
+        colors = self.get_product_colors(product.product_id)
         categories = self.get_product_categories(product.product_id)
         photos = self.get_product_photos(product.product_id)
-        return product_to_json(product, categories, photos)
+        return product_to_json(product, categories, photos, colors)
 
     async def create_product(self, product: ProductCreate, photos: List[UploadFile]) -> dict:
+
         Session = self.engine.create_session()
         with Session() as session:
             product_id = self.create_product_info(session, product)
             self.create_product_categories(session, product_id, product.categories)
             await self.create_product_photos(session, product_id, photos)
         Session.commit()
+        Session.remove()
+
         return self.get_product(product_id)
 
-    def create_product_info(self, session, product: ProductCreate) -> int:
+    def create_product_info(self, product: ProductCreate) -> int:
         new_product = Product(
             seller_id=product.seller_id,
             name=product.name,
@@ -119,32 +144,44 @@ class ProductService:
             total_price=product.total_price,
             sale_type=product.sale_type
         )
-        session.add(new_product)
-        session.commit()
+
+        Session = self.engine.create_session()
+        with Session() as session:
+            session.add(new_product)
+        Session.commit()
+        Session.remove()
+
         return new_product.product_id
 
-    def create_product_categories(self, session, product_id: int, categories: List[int]) -> None:
-        for category_id in categories:
-            new_product_category = ProductCategory(
-                product_id=product_id,
-                category_id=category_id
-            )
-            session.add(new_product_category)
-        session.commit()
+    def create_product_categories(self, product_id: int, categories: List[int]) -> None:
 
-    async def create_product_photos(self, session, product_id: int, photos: List[UploadFile]) -> None:
+        Session = self.engine.create_session()
+        with Session() as session:
+            for category_id in categories:
+                new_product_category = ProductCategory(
+                    product_id=product_id,
+                    category_id=category_id
+                )
+                session.add(new_product_category)
+        Session.commit()
+        Session.remove()
+
+    async def create_product_photos(self, product_id: int, photos: List[UploadFile]) -> None:
         if not os.path.exists(f"/backend/static/{product_id}"):
             os.mkdir(f"/backend/static/{product_id}")
 
-        for photo in photos:
-            photo_url = f"/backend/static/{product_id}/{uuid.uuid4()}.{photo.filename.split('.')[-1]}"
-            with open(photo_url, "wb") as image:
-                content = await photo.read()
-                image.write(content)
-                image.close()
-            new_photo = Photo(
-                product_id=product_id,
-                photo_url=photo_url
-            )
-            session.add(new_photo)
-        session.commit()
+        Session = self.engine.create_session()
+        with Session() as session:
+            for photo in photos:
+                photo_url = f"/backend/static/{product_id}/{uuid.uuid4()}.{photo.filename.split('.')[-1]}"
+                with open(photo_url, "wb") as image:
+                    content = await photo.read()
+                    image.write(content)
+                    image.close()
+                new_photo = Photo(
+                    product_id=product_id,
+                    photo_url=photo_url
+                )
+                session.add(new_photo)
+        Session.commit()
+        Session.remove()
