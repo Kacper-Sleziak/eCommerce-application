85474abeaa5a42fff29d9aac1e785f6e59f474a5
oluchna <olek@rafkos.net>
2023-05-17 19:00:34 +0200

    Contact page added

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,46 +1,33 @@
-import React from 'react'
-import {
-  Button,
-  Card,
-  FormControl,
-  InputLabel,
-  Input,
-  FormHelperText,
-  Typography,
-} from '@mui/material'
+import '../styles/pages/contact.css'
+import { Button, Card, Typography, TextField } from '@mui/material'
 
 const Contact = () => {
   return (
-    <Card>
-      <Typography variant="h4">
-        If you have any questions, feel free to contact us!{' '}
-      </Typography>
-      <FormControl>
-        <InputLabel>Message</InputLabel>
-        <Input />
-        <FormHelperText>Enter the message</FormHelperText>
-      </FormControl>
-      <FormControl>
-        <InputLabel>Your email</InputLabel>
-        <Input />
-        <FormHelperText>Enter your email</FormHelperText>
-      </FormControl>
-      <Button
-        variant="outlined"
-        sx={{
-          color: '#000',
-          background: '#fff',
-          border: '2px solid #000 !important',
-          fontWeight: '500',
-          fontStyle: 'normal',
-          fontSize: '20px',
-          lineHeight: '23px',
-          textTransform: 'none',
-          padding: '0.5rem 1rem',
-        }}
-      >
-        Send
-      </Button>
+    <Card sx={{ maxWidth: '50%' }}>
+      <div className="marginAll">
+        <Typography variant="h4">If you have any questions,</Typography>
+        <Typography variant="h4">feel free to contact us!</Typography>
+      </div>
+      <div className="formFlex">
+        <TextField id="message" multiline label="Message" variant="outlined" />
+        <TextField id="email" label="Your email" variant="outlined" />
+        <Button
+          variant="outlined"
+          sx={{
+            color: '#000',
+            background: '#fff',
+            border: '2px solid #000 !important',
+            fontWeight: '500',
+            fontStyle: 'normal',
+            fontSize: '20px',
+            lineHeight: '23px',
+            textTransform: 'none',
+            padding: '0.5rem 1rem',
+          }}
+        >
+          Send
+        </Button>
+      </div>
     </Card>
   )
 }
